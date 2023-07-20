200769f1ad43e975c00387d21ab61ed2192b7e2f
Soab42 <108150913+Soab42@users.noreply.github.com>
2023-07-20 13:59:12 +0600

    search

--- features/api/apiSlice.js
+++ features/api/apiSlice.js
@@ -3,8 +3,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    // baseUrl: "https://pmkbd.netlify.app/api",
-    baseUrl: "http://localhost:3000/api",
+    baseUrl: "https://pmkbd.netlify.app/api",
+    // baseUrl: "http://localhost:3000/api",
   }),
   tagTypes: [],
   keepUnusedDataFor: 10,
