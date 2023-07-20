91275732f83983446d8918d58b92ef3504e1fdd3
Soab42 <108150913+Soab42@users.noreply.github.com>
2023-07-20 13:58:03 +0600

    search

--- components/main/SearchComponent.js
+++ components/main/SearchComponent.js
@@ -10,42 +10,15 @@ import { useGetDatabaseAllQuery } from "@features/database/dbApi";
 
 export default function LeftComponent() {
   const search = useSelector((state) => state.filter.search);
-  // const [pathName, setpathName] = useState("");
+  const [pathName, setpathName] = useState("circuler");
   const { data, isError, isLoading, isSuccess } = useGetDatabaseAllQuery();
   console.log(data);
-  // Function to add the "pathname" property to each child object
-  function addPathname(obj, parentKey) {
-    for (const key in obj) {
-      if (typeof obj[key] === "object") {
-        const currentKey = parentKey ? parentKey + "." + key : key;
-        obj[key].pathname = currentKey;
-        addPathname(obj[key], currentKey);
-      }
-    }
-  }
 
-  // Loop through the main object and add the "pathname" property to each array
-  for (const key in mainObject) {
-    addPathname(mainObject[key], key);
-  }
-
-  // Function to get all objects from the nested structure into an array
-  function getAllObjects(obj, result = []) {
-    for (const key in obj) {
-      if (typeof obj[key] === "object") {
-        if (Array.isArray(obj[key].array)) {
-          result.push(...obj[key].array);
-        }
-        getAllObjects(obj[key], result);
-      }
-    }
-    return result;
-  }
   // Create an array containing all objects from the nested structure
-  const allObjectsArray = getAllObjects(data);
-  console.log(allObjectsArray);
+  // const allObjectsArray = getAllObjects(data);
+  // console.log(allObjectsArray);
 
-  const filteredData = AllData?.filter(
+  const filteredData = data?.circuler.filter(
     (item) => item?.tag?.includes(search) || item?.createdAt?.includes(search)
     // search === ""
     // console.log(item)
