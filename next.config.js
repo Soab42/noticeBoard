386f9abd921a75ed2e3b97339ca36f3a38270ff9
Soab42 <108150913+Soab42@users.noreply.github.com>
2023-07-28 12:46:21 +0600

    update

--- next.config.js
+++ next.config.js
@@ -9,7 +9,10 @@ const nextConfig = {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "pmkbd.netlify.app" }, // replace this your actual origin
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://pmkbd.netlify.app",
+          }, // replace this your actual origin
           {
             key: "Access-Control-Allow-Methods",
             value: "GET,DELETE,PATCH,POST,PUT",
