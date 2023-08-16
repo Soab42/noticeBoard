fa3fc9179ee33f9c8b89fbd073b51b16da94c30a
Soab42 <108150913+Soab42@users.noreply.github.com>
2023-08-16 15:18:40 +0600

    testing loan api

--- app/api/loan/route.js
+++ app/api/loan/route.js
@@ -188,47 +188,47 @@ async function scrapeData(username, password, memberId) {
       .filter((obj) => obj !== null);
     console.log(loanSchedule);
     //calculate loan schedule details
-    const rate = loanDetails.rate.slice(0, 2);
-    for (let i = 0; i < loanSchedule.length; i++) {
-      let currentLoan = loanSchedule[i];
-      let previousLoan = loanSchedule[i - 1] || {
-        closingOutstanding: loanDetails.loanAmount,
-        date: loanDetails.disburseDate ?? new Date(),
-      };
+    // const rate = loanDetails.rate.slice(0, 2);
+    // for (let i = 0; i < loanSchedule.length; i++) {
+    //   let currentLoan = loanSchedule[i];
+    //   let previousLoan = loanSchedule[i - 1] || {
+    //     closingOutstanding: loanDetails.loanAmount,
+    //     date: loanDetails.disburseDate ?? new Date(),
+    //   };
 
-      //day Calculation
+    //   //day Calculation
 
-      function calculateDaysDifference(startDateStr, endDateStr) {
-        const startDate = moment(
-          startDateStr,
-          ["YYYY-MM-DD", "DD/MM/YY"],
-          true
-        );
-        const endDate = moment(endDateStr, ["YYYY-MM-DD", "DD/MM/YY"], true);
+    //   function calculateDaysDifference(startDateStr, endDateStr) {
+    //     const startDate = moment(
+    //       startDateStr,
+    //       ["YYYY-MM-DD", "DD/MM/YY"],
+    //       true
+    //     );
+    //     const endDate = moment(endDateStr, ["YYYY-MM-DD", "DD/MM/YY"], true);
 
-        if (!startDate.isValid() || !endDate.isValid()) {
-          return NaN;
-        }
+    //     if (!startDate.isValid() || !endDate.isValid()) {
+    //       return NaN;
+    //     }
 
-        const daysDifference = endDate.diff(startDate, "days");
-        return daysDifference;
-      }
+    //     const daysDifference = endDate.diff(startDate, "days");
+    //     return daysDifference;
+    //   }
 
-      const days = calculateDaysDifference(previousLoan.date, currentLoan.date);
+    //   const days = calculateDaysDifference(previousLoan.date, currentLoan.date);
 
-      currentLoan.interest = (
-        previousLoan.closingOutstanding *
-        (rate / (365 * 100)) *
-        days
-      ).toFixed(0); // Example interest calculation
-      currentLoan.principle =
-        Number(currentLoan.installment.replace(/,/, "")) -
-        Number(currentLoan.interest);
+    //   currentLoan.interest = (
+    //     previousLoan.closingOutstanding *
+    //     (rate / (365 * 100)) *
+    //     days
+    //   ).toFixed(0); // Example interest calculation
+    //   currentLoan.principle =
+    //     Number(currentLoan.installment.replace(/,/, "")) -
+    //     Number(currentLoan.interest);
 
-      // Calculate closing outstanding
-      currentLoan.closingOutstanding =
-        previousLoan?.closingOutstanding - currentLoan.principle;
-    }
+    //   // Calculate closing outstanding
+    //   currentLoan.closingOutstanding =
+    //     previousLoan?.closingOutstanding - currentLoan.principle;
+    // }
 
     // const rate = parseFloat(loanDetails.rate) || 0; // Parse the rate to a floating-point number
     // const loanAmount = parseFloat(loanDetails.loanAmount) || 0; // Parse the loanAmount to a floating-point number
